477516d349ecd539cd06c3978179ee4adbcab7e6
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-01-31 15:10:12 +0600

    Configure firebase and add 2 component skeleton
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD6nIO6RzjdBZmpvbLSyxYUest4qYy4vEA',
+  authDomain: 'react-crud-9a036.firebaseapp.com',
+  databaseURL: 'https://react-crud-9a036-default-rtdb.firebaseio.com',
+  projectId: 'react-crud-9a036',
+  storageBucket: 'react-crud-9a036.appspot.com',
+  messagingSenderId: '102444288803',
+  appId: '1:102444288803:web:6e365c454597c7e7ed9a96',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
